510e3a2d8441920b42bf3b32af622faf9549e9a1
Vicky McDermott <vickymmcd@gmail.com>
2017-03-01 19:51:57 -0500

    Finishing up toolbox

--- frequency.py
+++ frequency.py
@@ -10,7 +10,19 @@ def get_word_list(file_name):
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+    f = open(file_name, 'r')
+    lines = f.readlines()
+    curr_line = 0
+    while lines[curr_line].find('Actus primus') == -1:
+        curr_line += 1
+    lines = lines[curr_line+1:]
+    words = []
+    for line in lines:
+        line = line.translate(line.maketrans('', '', string.punctuation))
+        line = line.lower()
+        line = line.strip()
+        words += line.split()
+    return words
 
 
 def get_top_n_words(word_list, n):
@@ -23,8 +35,17 @@ def get_top_n_words(word_list, n):
     returns: a list of n most frequently occurring words ordered from most
     frequently to least frequentlyoccurring
     """
-    pass
+    commonest_words = []
+    wordfreq = [word_list.count(p) for p in word_list]
+    word_counts = dict(zip(word_list, wordfreq))
+    ordered_by_frequency = sorted(word_counts, key=word_counts.get,
+                                  reverse=True)
+    for i, word in enumerate(ordered_by_frequency):
+        commonest_words.append(word)
+        if i > n:
+            return commonest_words
+
 
 if __name__ == "__main__":
     print("Running WordFrequency Toolbox")
-    print(string.punctuation)
+    print(get_top_n_words(get_word_list('shakespeare.txt'), 100))
